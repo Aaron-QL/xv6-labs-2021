18639095b5042aef3d4573c149901fef63948b0a
Aaron Qin <aka.qin@gmail.com>
2022-07-02 11:12:45 +0800

    optmize sleep

--- user/sleep.c
+++ user/sleep.c
@@ -1,12 +1,28 @@
 #include "../kernel/types.h"
 #include "../user/user.h"
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2) {
-        fprintf(2, "Usage: sleep <seconds>\n");
-        exit(1);
+int parse_int(const char* arg) {
+    const char* p = arg;
+    for ( ; *p ; p++ ) {
+        if ( *p < '0' || *p > '9' ) {
+            return -1;
+        }
     }
-    sleep(atoi(argv[1]));
+    return atoi(arg);
+}
+
+int main(int argc,char** argv) {
+    int time;
+    if (argc != 2) {
+        printf("you must input one argument only\n");
+        exit(0);
+    }
+
+    time = parse_int(argv[1]);
+    if (time < 0) {
+        printf("error argument : %s\n",argv[1]);
+        exit(0);
+    }
+    sleep(time);
     exit(0);
 }
